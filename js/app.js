b4a561f2877a39f9dd4627dd32bb8b4400222858
Oghuz Rustamli <91325870+oghuzrustamli@users.noreply.github.com>
2025-06-26 00:49:09 +0400

    Delete js directory

--- js/app.js
+++
@@ -1,7 +0,0 @@
-// Initialize services
-console.log('Initializing services...');
-const mapService = new MapService();
-const newsService = new NewsService(mapService);
- 
-// No automatic news fetching or interval updates
-console.log('Application initialized - use refresh button to fetch news'); 
